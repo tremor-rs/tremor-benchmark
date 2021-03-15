891127bd41581776a89446a54f0dcff4e850778e
Akshat Agarwal <humancalico@disroot.org>
2021-03-15 12:35:26 +0000

    chore: add clippy lints
    Signed-off-by: Akshat Agarwal <humancalico@disroot.org>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#![deny(clippy::pedantic)]
+#![deny(clippy::cargo)]
+#![deny(clippy::perf)]
+#![deny(clippy::complexity)]
+#![deny(clippy::nursery)]
+#![deny(clippy::style)]
+
 fn main() {
     println!("Hello, world!");
 }
