081eae19dc3e3690d18f65b028cc2fafeac86711
Akshat Agarwal <humancalico@disroot.org>
2021-03-15 20:36:30 +0530

    refactor: add color-eyre as a dependency
    Signed-off-by: Akshat Agarwal <humancalico@disroot.org>

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@
 #![deny(clippy::nursery)]
 #![deny(clippy::style)]
 
-fn main() {
+use color_eyre::eyre::Result;
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
