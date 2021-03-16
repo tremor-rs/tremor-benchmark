3a7d63d48e3a931e14a773bf8697366e7abf5642
Akshat Agarwal <humancalico@disroot.org>
2021-03-16 06:56:38 +0530

    feat: introduce clap as a dependency
    Signed-off-by: Akshat Agarwal <humancalico@disroot.org>

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,23 @@
 #![deny(clippy::nursery)]
 #![deny(clippy::style)]
 
+use clap::{crate_authors, crate_version, Clap};
 use color_eyre::eyre::Result;
+
+use std::path::PathBuf;
+
+#[derive(Clap)]
+#[clap(version = crate_version!(), author = crate_authors!())]
+struct Opts {
+    root: PathBuf,
+}
 
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    println!("The root directory of tremor is {:?}", opts.root);
 
     Ok(())
 }
