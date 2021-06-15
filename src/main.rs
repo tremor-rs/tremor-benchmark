a5eaeaefe3b070974e6eb13f27858acd00fe96f5
Akshat Agarwal <humancalico@disroot.org>
2021-06-15 15:48:05 +0530

    temporary commit should be split into and into multiple commits and force pushed later
    Signed-off-by: Akshat Agarwal <humancalico@disroot.org>

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,8 @@ struct Opts {
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-    // TODO check for dependencies like docker, docker-compose
+    // TODO check for dependencies like docker, docker-compose and git
+    // TODO should be able to use other container runtimes other than docker compose like podman
 
     let opts: Opts = Opts::parse();
 
@@ -35,9 +36,69 @@ fn main() -> Result<()> {
     // from the root of the project since that is the most likely scenario but that's not ideal
     env::set_current_dir(original_working_directory.join("docker")).expect("failed to change the current directory to docker/. Are you running this program from the root directory");
 
+    // TODO disable networking in the docker container
+
     // build Docker Image
-    // FIXME this assumes that this process is run from the root directory
-    // TODO actually run this
+    // FIXME this assumes that this process is run from the docker/ directory
+    fs::write("Dockerfile", String::from(
+    r#"# Although I still don't really get why we have to recompile it
+# Why we shouldn't use the edge image?
+#   *  Because that might be a point of failure for eg what happends when the image is not pushed
+#      that would lead to a previous commit being used. But we can add an additional check that
+#      makes sure whatever commit is used to build the image is same as the commit received from
+#      webhooks so that isn't really a problem.
+# Why we should use the edge image?
+#   *  Because that would prevent us from recompiling the project again and again. We currently
+#      recompile tremor a ton of times in tremor and it would be great if we could avoid that.
+FROM rust:1.52.1 as builder
+
+# TODO ensure wget is present
+# TODO figure out a way to get commit hash here
+# We planned to disable networking inside the docker containerat first but now we are doing this so that
+# can't be done anymore
+# TODO figure out a way by which we can allow networking in docker for certain sites only
+RUN wget https://github.com/tremor-rs/tremor-runtime/archive/COMMITHASH.tar.gz
+
+RUN tar -xf COMMITHASH.tar.gz
+
+RUN rm COMMITHASH.tar.gz
+
+WORKDIR tremor-runtime-COMMITHASH/
+
+ENV RUSTFLAGS="-C target-feature=+avx,+avx2,+sse4.2"
+
+# install dependencies
+RUN apt-get update \
+    && apt-get install -y libclang-dev cmake
+
+RUN cargo build -p tremor-cli --release
+
+FROM debian:buster-slim
+
+RUN useradd -ms /bin/bash tremor
+
+RUN apt-get update \
+    && apt-get install -y libssl1.1 libcurl4 libatomic1 tini curl
+
+# Copy the binary to /usr/local/bin
+COPY --from=builder tremor-runtime-COMMITHASH/target/release/tremor /usr/local/bin/
+
+# stdlib
+RUN mkdir -p /usr/share/tremor/lib
+COPY --from=builder tremor-runtime-COMMITHASH/tremor-script/lib /usr/share/tremor/lib
+
+# setting TREMOR_PATH
+# /usr/local/share/tremor - for host-specific local tremor-script modules and libraries, takes precedence
+# /usr/share/tremor/lib - place for the tremor-script stdlib
+ENV TREMOR_PATH="/usr/local/share/tremor:/usr/share/tremor/lib"
+
+RUN cd ..
+
+COPY benchmarks/ .
+
+CMD [ "tremor", "test", "bench", "/bench", "-o" ]
+"#).replace("COMMITHASH", &opts.commit_hash))?;
+
     // add the commit hash as tag
     Command::new("docker")
         .args(&[
@@ -55,6 +116,8 @@ fn main() -> Result<()> {
     let report_path = Path::new("report.json").canonicalize()?;
 
     // write a docker-compose.yml file
+    // TODO read the docker-compose.yml file to string
+    // TODO use the docker copy command to copy the report.json file instead of doing this
     fs::write(
         "docker-compose.yml",
         format!(
@@ -71,6 +134,8 @@ services:
             short_commit_hash
         ),
     )?;
+
+    // TODO add some sort of a timeout
 
     // run benchmarks inside docker image and store it in a report.json
     Command::new("docker-compose").arg("up").output()?;
@@ -95,13 +160,48 @@ services:
         ),
     )?;
 
-    // TODO remove report.json file
+    // remove report.json file
+    fs::remove_file(Path::new("docker").join("Dockerfile"))?;
 
-    // TODO remove docker-compose.yml
+    // remove report.json file
+    fs::remove_file(Path::new("docker").join("report.json"))?;
 
-    // TODO remove docker image
+    // remove docker-compose.yml
+    fs::remove_file(Path::new("docker").join("docker-compose.yml"))?;
 
-    // TODO remove docker container
+    // remove everything docker
+    // FIXME this is probably very bad
+    Command::new("docker")
+        .args(&["system", "prune", "--all"])
+        .output()?;
+
+    // TODO push the data to a the github repo
+    // git config user.email 81628356+tremorbot@users.noreply.github.com
+    Command::new("git")
+        .args(&[
+            "config",
+            "user.email",
+            "81628356+tremorbot@users.noreply.github.com",
+        ])
+        .output()?;
+    // git config user.name "tremorbot"
+    Command::new("git")
+        .args(&["config", "user.name", "tremorbot"])
+        .output()?;
+    // read TREMORBOT_PAT from environment
+    // git add data/data.json data/recent.json
+    Command::new("git")
+        .args(&["add", "data/data.json", "data/recent.json"])
+        .output()?;
+    // git commit --message "chore(data): update benchmarks for SHORTCOMMITHASH"
+    Command::new("git")
+        .args(&[
+            "commit",
+            &format!("chore(data): update for {}", short_commit_hash),
+        ])
+        .output()?;
+    // git push
+    Command::new("git").arg("push").output()?;
 
     Ok(())
 }
--- webhooks/index.js
+++ webhooks/index.js
@@ -1,12 +1,43 @@
-const express = require("express");
+// TODO RiiR
 const { spawnSync } = require("child_process");
+const crypto = require("crypto");
+
+const express = require("express");
+require('dotenv').config();
+
 const app = express();
 
-app.use(express.json());
+app.use(express.json({
+  verify: (req, res, buf, encoding) => {
+    if (buf && buf.length) {
+      req.rawBody = buf.toString(encoding || 'utf8');
+    }
+  },
+}));
 
-app.post("/payload", function (req, res) {
+const sigHeaderName = 'X-Hub-Signature-256'
+const sigHashAlg = 'sha256';
+
+function verify(req, res, next) {
+  if (!req.rawBody) {
+    return next('Request body empty')
+  }
+  const sig = Buffer.from(req.get(sigHeaderName) || '', 'utf8')
+  const hmac = crypto.createHmac(sigHashAlg, process.env.SECRET)
+  const digest = Buffer.from(sigHashAlg + '=' + hmac.update(req.rawBody).digest('hex'), 'utf8')
+  if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
+    return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`)
+  }
+
+  next()
+}
+
+app.post("/payload", verify, function (req, res) {
   if (req.body.action === "completed") {
     const commit_hash = req.body.check_suite.head_commit.id;
+    // respond to the post request
+    res.status(200).send(`Received a request for the commit ${commit_hash} and request body was signed`);
+    // start the benchmark pipeline
     const child = spawnSync("./target/release/tremor-benchmark", [
       "./data/data.json",
       "./data/recent.json",
@@ -22,5 +53,12 @@ app.post("/payload", function (req, res) {
   }
 });
 
-// TODO this should not be hardcoded
-app.listen(9247, '0.0.0.0');
+app.use((err, req, res, next) => {
+  if (err) console.error(err)
+  res.status(403).send('Request body was not signed or verification failed')
+})
+
+// should this be hardcoded?
+const port = 9247;
+const address = '0.0.0.0';
+app.listen(port, address, () => console.log(`Listening on port ${address}:${port}`));
