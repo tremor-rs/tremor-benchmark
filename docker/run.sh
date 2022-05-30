a409e893b3f1651f7ee5931883eb543dd78e88a7
Heinz N. Gies <heinz@licenser.net>
2022-05-30 13:58:42 +0200

    Some updates for 0.12
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,2 @@
-tremor test bench tremor-cli/tests/bench -o "${1}.json" > tremor.log
+TREMOR_PATH="$TREMOR_PATH:$PWD/tremor-cli/tests/lib" tremor test bench tremor-cli/tests/bench -o "${1}.json" > tremor.log
 cat "${1}.json"
